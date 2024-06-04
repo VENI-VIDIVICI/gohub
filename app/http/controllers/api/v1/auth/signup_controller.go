15320d0d63a52c370ff24b76e58b632bfc93dfed
qi <1311735850@qq.com>
2024-06-04 22:30:35 +0800

    验证请求

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -6,6 +6,7 @@ import (
 
 	v1 "github.com/VENI-VIDIVICI/gohub/app/http/controllers/api/v1"
 	"github.com/VENI-VIDIVICI/gohub/app/models/user"
+	"github.com/VENI-VIDIVICI/gohub/app/requests"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,17 +15,20 @@ type SignupController struct {
 }
 
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
-
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-	request := PhoneExistRequest{}
-
+	request := requests.SignupPhoneExistRequest{}
 	if err := c.ShouldBindJSON(&request); err != nil {
 		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
 			"error": err.Error(),
 		})
 		fmt.Println(err.Error())
+		return
+	}
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	if len(errs) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": errs,
+		})
+		fmt.Println(errs)
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
